ae7938ce157a7537ab2bd2ca042933406a17c34e
Shivansh Pratap Singh <shivanshpratapsingh0807@gmail.com>
2025-01-13 16:09:07 +0530

    card.jsx modified

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -11,8 +11,10 @@ const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
 ))
 CardHeader.displayName = "CardHeader"
 
-const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
-  <h3 ref={ref} className={cn("text-2xl font-semibold leading-none tracking-tight", className)} {...props} />
+const CardTitle = React.forwardRef(({ className, children, ...props }, ref) => (
+  <h3 ref={ref} className={cn("text-2xl font-semibold leading-none tracking-tight", className)} {...props}>
+    {children}
+  </h3>
 ))
 CardTitle.displayName = "CardTitle"
 
